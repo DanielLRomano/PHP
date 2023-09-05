568deb4eaca93b4f45b27b70a0a8e91af5262379
DanielLRomano <romanodaniel1012@gmail.com>
2023-09-04 21:56:30 -0300

    04/09

---
+++ exemploConexao/conexao.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Dados de conexão com o banco de dados 
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "banco";
+
+    // Criar a conexão com o BD
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+?>
