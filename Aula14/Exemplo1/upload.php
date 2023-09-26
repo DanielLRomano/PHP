b5c572b3ab25caa6d7894592b99ffe04021d0a2e
DanielLRomano <romanodaniel1012@gmail.com>
2023-09-25 21:45:43 -0300

    25/09

---
+++ Aula14/Exemplo1/upload.php
@@ -0,0 +1,16 @@
+<?php
+
+// pasta onde quer salvar os arquivos (obs: permissões de escrita habilitadas)
+$dir = "imagens/";
+
+// recebendo o arquivo multipart
+$file = $_FILES["arquivo"];
+
+// move o arquivo da pasta temporaria de upload para a pasta de destino
+if (move_uploaded_file($file["tmp_name"], $dir . $file["name"])) {
+    echo "Arquivo enviado com sucesso!";
+} else {
+    echo "Erro, o arquivo não pode ser enviado.";
+}
+
+?>
